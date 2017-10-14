2a8a4e5fb4b1f404396c1c91ad8c54e220905cd3
Louise Hayes <louise@deewhy.ie>
2017-10-14 16:23:26 -0400

    crystal images changed

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -14,7 +14,7 @@ var crystalButtonDiv = document.getElementById("crystalButtonDiv");
 var totalScoreTitleDiv = document.getElementById("totalScoreTitleDiv");
 var totalScoreNumberDiv = document.getElementById("totalScoreNumberDiv");
 
-// define crystal object
+// define crystal object to come back to if time
 var crystal = {
     crystalFunction: function () {
         totalScore += crystalScore;
@@ -26,6 +26,8 @@ var crystal = {
 
     //come back to this 
     // totalScoreCheck: function (event) {}
+
+    imagesUrl: ["assets/images/pink.jpg", "assets/images/bluecrystal.jpg", "assets/images/greencrystalresized.jpg", "assets/images/pinkcrystal.jpg"]
 };
 
 //Functions *****************************************
@@ -53,75 +55,75 @@ function getRandmoNo(posIndex) {
 
 
 
-    function init() {
-        var scoreToGet = getTargetScore();
-        console.log(scoreToGet);
-        $("#totalScoreTitleDiv").text("Your Total Score Is: ");
-        $("#totalScoreNumberDiv").text(0);
-        $("#winCounterDiv").text("Wins : " + winCntr);
-        $("#lossesCounterDiv").text("Losses: " + lossesCntr);
-        // for loop for every crystal number required:
+function init() {
+    var scoreToGet = getTargetScore();
+    console.log(scoreToGet);
+    $("#totalScoreTitleDiv").text("Your Total Score Is: ");
+    $("#totalScoreNumberDiv").text(0);
+    $("#winCounterDiv").text("Wins : " + winCntr);
+    $("#lossesCounterDiv").text("Losses: " + lossesCntr);
+    // for loop for every crystal number required:
 
-        for (var i = 0; i < 4; i++) {
-            getRandmoNo(i);
-            // dynamically generate crystal imageCrystal
+    for (var i = 0; i < 4; i++) {
+        getRandmoNo(i);
+        // dynamically generate crystal imageCrystal
 
-            var imageCrystal = $("<img>");
+        var imageCrystal = $("<img>");
 
-            // First each crystal will be given the class ".crystalimageDiv".
-            // This will allow the CSS to take effect.
-            imageCrystal.addClass("crystalimageDiv");
+        // First each crystal will be given the class ".crystalimageDiv".
+        // This will allow the CSS to take effect.
+        imageCrystal.addClass("crystalimageDiv");
 
-            // Each imageCrystal will be given a src link to the crystal image
-            imageCrystal.attr("src", "assets/images/pink.jpg");
+        // Each imageCrystal will be given a src link to the crystal image
+        imageCrystal.attr("src", crystal.imagesUrl[i]);
 
-            // Each imageCrystal will be given a data attribute called data-crystalValue.
-            // This data attribute will be set equal to the array value.
-            // imageCrystal.attr("data-crystalvalue", crystal.crystalNumberArray[i]);
-            var randomNoInObject = [];
-            randomNoInObject[i] = crystal.crystalNumberArray[i];
-            imageCrystal.attr("data-crystalvalue", randomNoInObject[i]);
+        // Each imageCrystal will be given a data attribute called data-crystalValue.
+        // This data attribute will be set equal to the array value.
+        // imageCrystal.attr("data-crystalvalue", crystal.crystalNumberArray[i]);
+        var randomNoInObject = [];
+        randomNoInObject[i] = crystal.crystalNumberArray[i];
+        imageCrystal.attr("data-crystalvalue", randomNoInObject[i]);
 
-            // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-            $("#imgCrystal").append(imageCrystal);
-        }
-        // end loop
-
-        $(".crystalimageDiv").on("click", function () {
-            // calls the crystal function and adds that crystal value to your totalScore
-            // checks if your totalscore is over the target score
-            // call crystal function and tally total score
-            // crystal.crystalFunction(crystalScore); to be refactored later
-            var crystalValue = ($(this).attr("data-crystalvalue"));
-            crystalValue = parseInt(crystalValue);
-            // We then add the crystalValue to the user's "counter" which is a global variable.
-            // Every click, from every crystal adds to the global counter.
-            totalScore += crystalValue;
-            $("#totalScoreNumberDiv").text(totalScore);
-            // All of the same game win-lose logic applies. So the rest remains unchanged.
-
-            if (totalScore === scoreToGet) {
-                winCntr++;
-                totalScore = 0;
-                $("#countersTitleDiv").html("You Win!");
-                $("#winCounterDiv").text("Wins : " + winCntr);
-                $("#totalScoreNumberDiv").text(totalScore);
-                scoreToGet = getTargetScore();
-
-            } else if (totalScore >= scoreToGet) {
-                lossesCntr++;
-                totalScore = 0;
-                $("#countersTitleDiv").text("You Loose!");
-                $("#lossesCounterDiv").text("Losses: " + lossesCntr);
-                $("#totalScoreNumberDiv").text(totalScore);
-                scoreToGet = getTargetScore();
-            }
-
-        });
-
+        // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
+        $("#imgCrystal").append(imageCrystal);
     }
-    
-$(document).ready(function () {    
+    // end loop
 
-init();
+    $(".crystalimageDiv").on("click", function () {
+        // calls the crystal function and adds that crystal value to your totalScore
+        // checks if your totalscore is over the target score
+        // call crystal function and tally total score
+        // crystal.crystalFunction(crystalScore); to be refactored later
+        var crystalValue = ($(this).attr("data-crystalvalue"));
+        crystalValue = parseInt(crystalValue);
+        // We then add the crystalValue to the user's "counter" which is a global variable.
+        // Every click, from every crystal adds to the global counter.
+        totalScore += crystalValue;
+        $("#totalScoreNumberDiv").text(totalScore);
+        // All of the same game win-lose logic applies. So the rest remains unchanged.
+
+        if (totalScore === scoreToGet) {
+            winCntr++;
+            totalScore = 0;
+            $("#countersTitleDiv").html("You Win!");
+            $("#winCounterDiv").text("Wins : " + winCntr);
+            $("#totalScoreNumberDiv").text(totalScore);
+            scoreToGet = getTargetScore();
+
+        } else if (totalScore >= scoreToGet) {
+            lossesCntr++;
+            totalScore = 0;
+            $("#countersTitleDiv").text("You Loose!");
+            $("#lossesCounterDiv").text("Losses: " + lossesCntr);
+            $("#totalScoreNumberDiv").text(totalScore);
+            scoreToGet = getTargetScore();
+        }
+
+    });
+
+}
+
+$(document).ready(function () {
+
+    init();
 });
